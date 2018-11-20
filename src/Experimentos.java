aa06eb883a4406250a301da497bcc2ccaa18e15a
cmunyozg <cmunyozg@administrador-ALDA>
2018-11-20 18:33:37 +0100

    Subida inicial

---
+++ src/Experimentos.java
@@ -0,0 +1,11 @@
+
+public class Experimentos {
+public static void main (String[]args) {
+	String coordenada = "A1";
+	int coordY = (int) (coordenada.charAt(0)) - 65;
+	int coordX = coordenada.charAt(1) -49;
+	
+	System.out.println(coordY);
+	System.out.println(coordX);
+}
+}
---
+++ src/Hundir.java
@@ -0,0 +1,165 @@
+import java.util.Scanner;
+
+public class Hundir {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		String nombre;
+		char[][] tableroJugador = new char[11][11];
+		char[][] tableroDisparosJugador = new char[11][11];
+		char[][] tableroPC = new char[11][11];
+		char[][] tableroDisparosPC = new char[11][11];
+
+		System.out.print("Introduce tu nombre: ");
+		nombre = entrada.next();
+
+		inicializarTablero(tableroJugador);
+		inicializarTablero(tableroDisparosJugador);
+		inicializarTablero(tableroPC);
+		inicializarTablero(tableroDisparosPC);
+
+		tableroPC[4][4] = 'B';
+		tableroPC[4][5] = 'B';
+		tableroJugador[3][2] = 'B';
+		tableroJugador[3][3] = 'B';
+		tableroJugador[5][2] = 'B';
+		tableroJugador[5][3] = 'B';
+
+		visualizarTablero(tableroPC, tableroDisparosPC);
+		System.out.println();
+		visualizarTablero(tableroJugador, tableroDisparosJugador);
+		disparoJugador(tableroJugador, tableroDisparosJugador, tableroPC);
+		disparoPC(tableroPC, tableroDisparosPC, tableroJugador);
+
+	}
+
+	// Este metodo coloca los barcos pasados como vector dentro del tablero del
+	// Jugador
+	public static void colocarBarcosJugador(char[][] tablero/* ,char[][] tableroDisparos */, int[] barcos) {
+		Scanner entrada = new Scanner(System.in);
+		char[] barco4 = { 'B', 'B', 'B', 'B' };
+		char[] barco3_1 = { 'B', 'B', 'B' };
+		char[] barco3_2 = { 'B', 'B', 'B' };
+		char[] barco3_3 = { 'B', 'B', 'B' };
+		char[] barco2_1 = { 'B', 'B' };
+		char[] barco2_2 = { 'B', 'B' };
+		char[] barco2_3 = { 'B', 'B' };
+		char[] barco2_4 = { 'B', 'B' };
+
+		System.out.println("Introduce las coordenadas del Portaviones");
+
+	}
+
+	// Método que implementa el disparo del jugador.
+	public static void disparoJugador(char[][] tableroJugador, char[][] tableroDisparosJugador, char[][] tableroPC) {
+		Scanner entrada = new Scanner(System.in);
+		boolean disparo = false;
+//		String y;
+		int coordY;
+		int coordX;
+		String coordenada;
+
+//		System.out.println("Introduce la coordenada Y [A-J]");
+//		y = entrada.next();
+//		System.out.println("Introduce la coordenada X [0-9]");
+//		coordX = entrada.nextInt() + 1;
+//		int coordY = y.charAt(0) - 65;
+
+		System.out.println("Introduce la coordenada del disparo. Ej [A1]");
+		coordenada = entrada.next();
+		coordY = (int) (coordenada.charAt(0)) - 65;
+		coordX = coordenada.charAt(1) -49;
+
+		if (tableroPC[coordY][coordX] != '~') {
+			tableroDisparosJugador[coordY][coordX] = 'T';
+			disparo = true;
+			// borrarPantalla();
+			visualizarTablero(tableroJugador, tableroDisparosJugador);
+		} else if (tableroPC[coordY][coordX] == '~') {
+			tableroDisparosJugador[coordY][coordX] = '*';
+			// borrarPantalla();
+			visualizarTablero(tableroJugador, tableroDisparosJugador);
+		}
+
+		if (disparo == true)
+			disparoJugador(tableroJugador, tableroDisparosJugador, tableroPC);
+
+	}
+
+	// Método que implementa el disparo del PC. COMPROBAR
+	public static void disparoPC(char[][] tableroPC, char[][] tableroDisparosPC, char[][] tableroJugador) {
+		boolean disparo = false;
+		int coordY;
+		int coordX;
+
+		do {
+			coordY = (int) (Math.random() * ((9 - 0 + 1) + 0));
+			coordX = (int) (Math.random() * (9 - 0 + 1) + 0);
+
+		} while (tableroDisparosPC[coordY][coordX] != '~');
+
+		if (tableroJugador[coordY][coordX] != '~') {
+			tableroDisparosPC[coordY][coordX] = 'T';
+			tableroJugador[coordY][coordX] = 'T';
+			disparo = true;
+			// borrarPantalla();
+			visualizarTablero(tableroPC, tableroDisparosPC);
+		} else if (tableroJugador[coordY][coordX] == '~') {
+			tableroDisparosPC[coordY][coordX] = '*';
+			tableroJugador[coordY][coordX] = '*';
+			// borrarPantalla();
+			visualizarTablero(tableroPC, tableroDisparosPC);
+		}
+
+		if (disparo == true)
+			disparoJugador(tableroPC, tableroDisparosPC, tableroJugador);
+
+	}
+
+	// Método que inicia los tableros desde cero.
+	public static void inicializarTablero(char[][] tablero) {
+		char letra = 'A';
+		char numero = '0';
+		tablero[tablero.length - 1][0] = ' ';
+		for (int fila = 0; fila < tablero.length - 1; fila++) {
+			tablero[fila][0] = letra;
+			letra++;
+		}
+		for (int fila = 0; fila < tablero.length; fila++) {
+			for (int columna = 1; columna < tablero[0].length; columna++) {
+				tablero[fila][columna] = '~';
+
+			}
+		}
+		for (int columna = 1; columna < tablero[0].length; columna++) {
+			tablero[10][columna] = numero;
+			numero++;
+		}
+	}
+
+	// Método que visualiza los tableros en pantalla.
+	public static void visualizarTablero(char[][] tablero, char[][] tableroDisparos) {
+		System.out.println("    Tablero Jugador" + "\t\t" + "   Tablero Disparos");
+		for (int fila = 0; fila < tablero.length; fila++) {
+			for (int columna = 0; columna < tablero[0].length; columna++) {
+				System.out.print(tablero[fila][columna] + " ");
+
+			}
+			System.out.print("\t\t");
+			for (int columna = 0; columna < tableroDisparos[0].length; columna++) {
+
+				System.out.print(tableroDisparos[fila][columna] + " ");
+
+			}
+			System.out.println();
+
+		}
+	}
+
+	// Método que borra la terminal.
+	public static void borrarPantalla() {
+		for (int i = 0; i < 50; i++) {
+			System.out.println();
+		}
+	}
+}
